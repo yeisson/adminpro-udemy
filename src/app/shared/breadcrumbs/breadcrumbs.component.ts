375e9e620533adb6d7bd3a9a0c08fe64b934c14f
Yeisson <yeisson@gmail.com>
2018-05-19 21:21:12 -0500

    Seccioón 8 Terminada

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,7 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  label = '';
+
+  constructor( private router: Router,
+              public title: Title,
+              public meta: Meta ) {
+
+     this.getDataRoute()
+    .subscribe( data => {
+
+      console.log( data );
+      this.label = data.titulo;
+      this.title.setTitle( 'AdminPro ' + data.titulo);  // Modificar el titulo de la pagina dinamicamente
+
+
+      const metaTag: MetaDefinition = {  // Modificar lo meta de la pagina dinamicamente
+        name: 'decription',
+        content: this.label,
+      };
+
+      this.meta.updateTag(metaTag);
+
+    });
+
+  }
+
+  getDataRoute() {
+    return  this.router.events
+    .filter( evento => evento instanceof ActivationEnd)   // para Filtrar solo los eventos del Activation End
+    .filter( (evento: ActivationEnd) => evento.snapshot.firstChild === null)  // para Filtrar solo el primero
+    .map( (evento: ActivationEnd) => evento.snapshot.data );
+  }
 
   ngOnInit() {
   }
