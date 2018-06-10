b27972df1d955e67dd10d7b330ae083a8e463fc6
Yeisson <yeisson@gmail.com>
2018-06-10 14:25:36 -0500

    Sección 16 Terminada

--- src/app/pages/medicos/medico.component.ts
+++ src/app/pages/medicos/medico.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Medico } from '../../models/medico.model';
+import { MedicoService } from '../../services/service.index';
+import { NgForm } from '@angular/forms';
+import { Hospital } from '../../models/hospital.model';
+import { HospitalService } from '../../services/service.index';
+import { ImagenPipe } from '../../pipes/imagen.pipe';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ModalUploadService } from '../../components/modal-upload/modal-upload.service';
 
 @Component({
   selector: 'app-medico',
@@ -7,9 +15,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoComponent implements OnInit {
 
-  constructor() { }
+  hospitales: Hospital[] = [];
+  medico: Medico = new Medico('', '', '', '', '');
+  hospital: Hospital = new Hospital('');
+
+  constructor(
+        public _medicoService: MedicoService,
+        public _hospitalService: HospitalService,
+        public router: Router,
+        public activatedRoute: ActivatedRoute,
+        public _modalUploadService: ModalUploadService
+  ) {
+
+    activatedRoute.params.subscribe( params => {
+      const id = params['id'];
+
+      if ( id !== 'nuevo' ) {
+        this.cargarMedico( id );
+      }
+
+    });
+
+   }
 
   ngOnInit() {
+
+    this._hospitalService.cargarHospitales()
+        .subscribe( (hospitales: any) => {
+           this.hospitales = hospitales.hospitales;
+        });
+
+    this._modalUploadService.notificacion
+            .subscribe( resp => {
+              this.medico.img = resp.medico.img;
+            });
+  }
+
+  cargarMedico( id: string ) {
+    this._medicoService.cargarMedico( id )
+          .subscribe( (medico: any) => {
+              this.medico = medico;
+              this.medico.hospital = medico.hospital._id;
+              this.cambioHospital( this.medico.hospital);
+          });
+  }
+
+
+  guardarMedico( f: NgForm) {
+    console.log( f.valid );
+    console.log ( f.value );
+
+    if ( f.invalid ) {
+      return;
+    }
+
+    this._medicoService.guardarMedico ( this.medico )
+            .subscribe( medico => {
+              this.medico._id = medico._id;
+              this.router.navigate(['/medico', medico._id]);
+            });
+
+  }
+
+  cambioHospital( id: string ) {
+
+    this._hospitalService.obtenerHospital( id )
+            .subscribe( (hospital: any) => {
+              this.hospital = hospital.hospital;
+            });
+
+  }
+
+  cambiarFoto( ) {
+    this._modalUploadService.mostrarModal( 'medicos', this.medico._id );
   }
 
 }
--- src/app/services/medico/medico.service.ts
+++ src/app/services/medico/medico.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { URL_SERVICIOS } from '../../config/config';
 import { UsuarioService } from '../usuario/usuario.service';
 import swal from 'sweetalert2';
+import { Medico } from '../../models/medico.model';
 
 @Injectable()
 export class MedicoService {
@@ -47,7 +48,45 @@ export class MedicoService {
             swal( 'Médico Borrado', 'Médico borrado correctamente', 'success');
             return resp;
           });
+  }
 
+  guardarMedico( medico: Medico) {
+
+    let url = URL_SERVICIOS + '/medico';
+
+    if ( medico._id ) {
+      // actualizando
+      url += '/' + medico._id;
+      url += '?token=' +  this._usuarioService.token;
+
+      return this.http.put( url, medico )
+              .map( (resp: any) => {
+                swal( 'Medico Actualizado', medico.nombre , 'success');
+                return resp.medico;
+              });
+
+
+    } else {
+      // Creando
+      url += '?token=' +  this._usuarioService.token;
+
+      return this.http.post( url, medico )
+              .map( (resp: any) => {
+                swal( 'Medico Creado', medico.nombre , 'success');
+                return resp.medico;
+              });
+    }
+
+  }
+
+  cargarMedico ( id: string ) {
+
+    const url = URL_SERVICIOS + '/medico/' + id;
+
+    return this.http.get( url )
+        .map( (resp: any) => {
+          return resp.medico;
+        }) ;
 
   }
 
